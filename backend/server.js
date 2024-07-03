144db2b56301c1d444e41aac751a6210483423ef
Waqas Ahmad <malikwaqas077@gmail.com>
2024-07-03 14:48:10 +0100

    chnaged cors settings

--- backend/server.js
+++ backend/server.js
@@ -10,8 +10,6 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-
-
 const parking_fee = [
   { id: 1, Fee: "UP TO 1 HR - £1" },
   { id: 2, Fee: "UP TO 2 HR - £2" },
@@ -46,7 +44,12 @@ const days = [
   { id: 5, Days: 5 },
 ];
 
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:5000', // Allow only this origin to access the resources
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allow these HTTP methods
+  allowedHeaders: ['Content-Type', 'Authorization'], // Allow these headers
+}));
+
 app.use(express.static(path.join(__dirname, '../dist')));
 
 app.get('/api/days', (req, res) => {
